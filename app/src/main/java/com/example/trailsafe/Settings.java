926b97f2ee777cf9158364bdad13e1e1202aadb5
derryodonovan <odonovde@tcd.ie>
2020-02-21 13:02:56 +0000

    Added Settings

---
+++ app/src/main/java/com/example/trailsafe/Settings.java
@@ -0,0 +1,16 @@
+package com.example.trailsafe;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+
+
+}
