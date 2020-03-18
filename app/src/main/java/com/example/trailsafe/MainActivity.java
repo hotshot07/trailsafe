9be3fa3c2cd51407ab040d5a7c3daad04d018430
Uchenna Udemezue <udemezuu@tcd.ie>
2020-03-18 14:49:55 +0000

    new button animations

--- app/src/main/java/com/example/trailsafe/MainActivity.java
+++ app/src/main/java/com/example/trailsafe/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 //import kotlinx.android.synthetic.main.activity_maps
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.View;
 import android.view.animation.Animation;
@@ -23,53 +24,87 @@ public class MainActivity extends AppCompatActivity {
     double x = 0;
     int y = 0;
 
+
+    public void TapToMove(View view) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Magic here
+            }
+        }, 10000); // Millisecond 1000 = 1 sec
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.move);
+        btn_next.startAnimation(animation);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Magic here
+            }
+        }, 1000); // Millisecond 1000 = 1 sec
+    }
+
+    public void TapToSlide(View view) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Magic here
+            }
+        }, 10000); // Millisecond 1000 = 1 sec
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.slide);
+        btn_profile.startAnimation(animation);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Magic here
+            }
+        }, 1000); // Millisecond 1000 = 1 sec
+    }
+
+    public void TapToSlideAgain(View view) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Magic here
+            }
+        }, 10000); // Millisecond 1000 = 1 sec
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.slide);
+        btn_settings.startAnimation(animation);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Magic here
+            }
+        }, 1000); // Millisecond 1000 = 1 sec
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn_next = (Button) findViewById(R.id.NextButton);
         btn_profile = (ImageButton) findViewById(R.id.Profile);
+        btn_settings = (ImageButton) findViewById(R.id.Settings);
         btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                while(x < 2000000){
-                    y++;
-                    x = System.currentTimeMillis();
-                }
+            public void onClick(final View view) {
+                TapToMove(view);
+
                 Intent intent = new Intent(MainActivity.this, MapsActivity.class);
                 startActivity(intent);
             }
         });
 
-        btn_settings = (ImageButton) findViewById(R.id.Settings);
         btn_settings.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                x = 0;
-                while(x < 2000000){
-                    y++;
-                    x = System.currentTimeMillis();
-                }
+            public void onClick(final View view) {
+                TapToSlideAgain(view);
+
                 Intent intent = new Intent(MainActivity.this, Settings.class);
                 startActivity(intent);
             }
         });
     }
 
-    public void TapToMove(View view) {
-        Animation animation = AnimationUtils.loadAnimation(this, R.anim.move);
-        btn_next.startAnimation(animation);
-    }
-
-    public void TapToBlink(View view) {
-        Animation animation = AnimationUtils.loadAnimation(this, R.anim.slide);
-        btn_settings.startAnimation(animation);
-    }
-
-    public void TapToSlide(View view) {
-        Animation animation = AnimationUtils.loadAnimation(this, R.anim.slide);
-        btn_profile.startAnimation(animation);
-    }
 
 
 
