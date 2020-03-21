2180a0b7cfcaa3442054ab8efd264296c1ab1717
derryodonovan <odonovde@tcd.ie>
2020-03-21 20:20:48 +0000

    Added Toolbar

--- app/src/main/java/com/example/trailsafe/MainActivity.java
+++ app/src/main/java/com/example/trailsafe/MainActivity.java
@@ -7,20 +7,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
 
 import java.math.BigInteger;
 
 
 public class MainActivity extends AppCompatActivity {
     private Button btn_next;
-    private ImageButton btn_settings;
-    private ImageButton btn_profile;
     double x = 0;
     int y = 0;
 
@@ -42,48 +45,18 @@ public class MainActivity extends AppCompatActivity {
         }, 1000); // Millisecond 1000 = 1 sec
     }
 
-    public void TapToSlide(View view) {
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Magic here
-            }
-        }, 10000); // Millisecond 1000 = 1 sec
-        Animation animation = AnimationUtils.loadAnimation(this, R.anim.slide);
-        btn_profile.startAnimation(animation);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Magic here
-            }
-        }, 1000); // Millisecond 1000 = 1 sec
-    }
 
-    public void TapToSlideAgain(View view) {
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Magic here
-            }
-        }, 10000); // Millisecond 1000 = 1 sec
-        Animation animation = AnimationUtils.loadAnimation(this, R.anim.slide);
-        btn_settings.startAnimation(animation);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // Magic here
-            }
-        }, 1000); // Millisecond 1000 = 1 sec
-
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         btn_next = (Button) findViewById(R.id.NextButton);
-        btn_profile = (ImageButton) findViewById(R.id.Profile);
-        btn_settings = (ImageButton) findViewById(R.id.Settings);
+
         btn_next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View view) {
@@ -94,17 +67,45 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btn_settings.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(final View view) {
-                TapToSlideAgain(view);
 
-                Intent intent = new Intent(MainActivity.this, Settings.class);
-                startActivity(intent);
-            }
-        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.example_menu, menu);
+        return true;
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case  R.id.item_profile:
+                Toast.makeText(this, "Item 1 selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case  R.id.item_settings:
+                Intent intent = new Intent(MainActivity.this, Settings.class);
+                startActivity(intent);
+                return true;
+
+            case  R.id.item2:
+                Toast.makeText(this, "Item 3 selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case  R.id.subitem1:
+                Toast.makeText(this, "Sub Item 1 selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case  R.id.subitem2:
+                Toast.makeText(this, "Sub Item 2 selected", Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
 
 
 
