b4da0abd10bc64c4ba329ec7bd28513c98ad5962
Mayank Arora <aroram@tcd.ie>
2020-04-03 03:57:18 +0100

    signup working

--- app/src/main/java/com/example/trailsafe/LoginActivity.java
+++ app/src/main/java/com/example/trailsafe/LoginActivity.java
@@ -5,12 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -65,12 +69,36 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 String email = mEmail.getText().toString();
                 String pass = mPassword.getText().toString();
-                if(!email.equals("") && !pass.equals("")){
-                    mAuth.signInWithEmailAndPassword(email,pass);
-                    toastMessage("Signing in...");
-                }else{
-                    toastMessage("You didn't fill in all the fields.");
+
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email is Required.");
+                    return;
                 }
+
+                if (TextUtils.isEmpty(pass)) {
+                    mPassword.setError("Password is Required.");
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }else {
+                            Toast.makeText(LoginActivity.this, "Error ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
+//                if(!email.equals("") && !pass.equals("")){
+//                    mAuth.signInWithEmailAndPassword(email,pass);
+//                    toastMessage("Signing in...");
+//                }else{
+//                    toastMessage("You didn't fill in all the fields.");
+//                }
             }
         });
 
